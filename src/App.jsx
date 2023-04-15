3ee2a14c574d3a0fd0b8d285607d00d357e21009
sabbirziauddin <sabbir.ziauddin@gmail.com>
2023-04-15 18:41:53 +0200

    banner image added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./Router/Routes/Routes";
+
+function App() {
+  
+
+  return (
+    <div data-theme="cupcake" className="max-w-7xl mx-auto">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App
